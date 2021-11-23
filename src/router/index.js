9c3b8b018430671f1f9ff3b31d954ed3d7094522
孙晓洁 <sunxiaojie@sunxiaojiedeMacBook-Air.local>
2021-11-23 21:05:36 +0800

    首次提交代码

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Echarts from "@/components/Echarts";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Echarts",
+      component: Echarts,
+    },
+  ],
+});
